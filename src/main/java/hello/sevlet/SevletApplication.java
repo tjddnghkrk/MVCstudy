f1011ac3aa005ebcba1cf0ea63235aa94f602200
tjddnghkrk <tjddnghkrk@gmail.com>
2022-02-18 00:03:25 +0900

    first

---
+++ src/main/java/hello/sevlet/SevletApplication.java
@@ -0,0 +1,15 @@
+package hello.sevlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan // 하위 디레토리 다 뒤져서 서블릿 자동 등록
+@SpringBootApplication
+public class SevletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SevletApplication.class, args);
+	}
+
+}
